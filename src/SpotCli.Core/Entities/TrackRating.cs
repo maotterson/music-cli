a636cdd8f4bd6d301b03afa37cb9dfde379dc41f
maotterson <otterson.2@gmail.com>
2022-06-03 00:36:45 -0400

    add value object for rating int

--- src/SpotCli.Core/Entities/TrackRating.cs
+++ src/SpotCli.Core/Entities/TrackRating.cs
@@ -9,6 +9,7 @@ namespace SpotCli.Core.Entities;
 public class TrackRating
 {
     public string Id { get; init; } = ""; // todo required keyword in c#11
+    public RatingVO? Rating { get; init; }
     public SpotifyIdVO? SpotifyId { get; init; }
     public ArtistVO? Artist { get; init; }
     public TrackVO? Track { get; init; }
---
+++ src/SpotCli.Core/ValueObjects/RatingVO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotCli.Core.ValueObjects;
+public class RatingVO
+{
+    private const int MIN_RATING = 1;
+    private const int MAX_RATING = 5;
+    public RatingVO(int value)
+    {
+        if (!IsValidRating(value))
+        {
+            throw new Exception($"Invalid rating. Value must be between {MIN_RATING} and {MAX_RATING}."); // todo more specific exception object
+        }
+        Value = value;
+    }
+
+    public int Value { get; init; }
+
+    private bool IsValidRating(int rating)
+    {
+        if(rating < MIN_RATING || rating > MAX_RATING)
+        {
+            return false;
+        }
+        return true;
+    }
+}
