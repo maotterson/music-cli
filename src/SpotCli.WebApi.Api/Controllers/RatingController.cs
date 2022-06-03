793a7979ee5505d0ddda1c3b41f90f245c511673
maotterson <otterson.2@gmail.com>
2022-06-03 15:20:07 -0400

    replace lambda simple function with webapi project template

---
+++ src/SpotCli.WebApi.Api/Controllers/RatingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace SpotCli.WebApi.Api.Controllers;
+
+[ApiController]
+[Route("rating/")]
+public class RatingController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return Ok("hello world");
+    }
+}
