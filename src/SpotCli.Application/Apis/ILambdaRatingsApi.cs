f50277fd957334b485c192721ac5053b3c668348
maotterson <otterson.2@gmail.com>
2022-06-08 01:29:27 -0400

    create refit api and response contract classes

---
+++ src/SpotCli.Application/Apis/ILambdaRatingsApi.cs
@@ -0,0 +1,9 @@
+﻿using Refit;
+using SpotCli.Application.Ratings.GetAllRatings;
+
+namespace SpotCli.Application.Apis;
+public interface ILambdaRatingsApi
+{
+    [Get("/")]
+    Task<IApiResponse<GetAllRatingsResponse>> GetAllRatings();
+}
