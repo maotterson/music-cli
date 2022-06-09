2d8f2f424bac52b63836cd4de2baebaebb6182a4
maotterson <otterson.2@gmail.com>
2022-06-08 21:49:53 -0400

    create contracts for get requests from track ratings

--- src/SpotCli.Application/Ratings/GetAllRatings/GetAllRatingsResponse.cs
+++ src/SpotCli.Application/Ratings/GetAllRatings/GetAllRatingsResponse.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using SpotCli.Application.Dto;
+using SpotCli.Application.Ratings.GetRating;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,4 +11,5 @@ using System.Threading.Tasks;
 namespace SpotCli.Application.Ratings.GetAllRatings;
 public class GetAllRatingsResponse
 {
+    public IEnumerable<GetRatingResponse> Ratings { get; init; } = Enumerable.Empty<GetRatingResponse>();
 }
--- src/SpotCli.Application/Ratings/GetRating/GetRatingResponse.cs
+++ src/SpotCli.Application/Ratings/GetRating/GetRatingResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,4 +8,16 @@ using System.Threading.Tasks;
 namespace SpotCli.Application.Ratings.GetRating;
 public class GetRatingResponse
 {
+    [JsonProperty("id")]
+    public string Id { get; init; }
+    [JsonProperty("spotify_id")]
+    public string SpotifyId { get; init; }
+    [JsonProperty("artist")]
+    public string Artist { get; init; }
+    [JsonProperty("album")]
+    public string Album { get; init; }
+    [JsonProperty("track")]
+    public string Track { get; init; }
+    [JsonProperty("rating")]
+    public int Rating { get; init; }
 }
